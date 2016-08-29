d830c03fb0ab156f16e9b55a4088ed894d752666
Dylan Honorez <dylan.honorez@goudengids.be>
2016-08-29 17:44:23 +0200

    Custom impl + integration test.

---
+++ src/main/java/com/axxes/persistence/repository/BookRepositoryCustom.java
@@ -0,0 +1,7 @@
+package com.axxes.persistence.repository;
+
+/**
+ * Created by dhonorez on 29/08/16.
+ */
+public interface BookRepositoryCustom {
+}
---
+++ src/main/java/com/axxes/persistence/repository/impl/BookRepositoryCustomImpl.java
@@ -0,0 +1,9 @@
+package com.axxes.persistence.repository.impl;
+
+import com.axxes.persistence.repository.BookRepositoryCustom;
+
+/**
+ * Created by dhonorez on 29/08/16.
+ */
+public class BookRepositoryCustomImpl implements BookRepositoryCustom {
+}
